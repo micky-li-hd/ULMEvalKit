840c17854e9b5e2e97ae610e4720de88010be6d0
CaraJ7 <1350074492@qq.com>
2025-10-12 11:06:49 +0800

    [Fix] Fix lint error in T2I-R1

--- ulmeval/ulm/t2ir1.py
+++ ulmeval/ulm/t2ir1.py
@@ -32,7 +32,7 @@ class T2IR1(BaseModel):
 
         except Exception as err:
             logging.critical(
-                'Please first clone T2IR1 from source codes in: https://github.com/CaraJ7/T2I-R1 and set up the environment variable T2IR1_ROOT as "T2I-R1/src/t2i-r1/src"')
+                'Please first clone T2IR1 from source codes in: https://github.com/CaraJ7/T2I-R1 and set up the environment variable T2IR1_ROOT as "T2I-R1/src/t2i-r1/src"')  # noqa: E501
             raise err
 
         self.vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
@@ -49,9 +49,9 @@ Provide a brief, precise visualization of all elements in the prompt. Your descr
 4. Be concise (50 words or less)
 5. Focus only on what's explicitly stated in the prompt
 6. Do not elaborate beyond the attributes or relationships specified in the prompt
-Do not miss objects. Output your visualization directly without explanation: '''
+Do not miss objects. Output your visualization directly without explanation: '''  # noqa: E501
 
-        self.system_prompt = 'You are a helpful assistant that receives an image prompt and generate a visualization of the prompt.'
+        self.system_prompt = 'You are a helpful assistant that receives an image prompt and generate a visualization of the prompt.'  # noqa: E501
 
         default_kwargs = dict(
             temperature=1,
@@ -103,7 +103,6 @@ Do not miss objects. Output your visualization directly without explanation: '''
         prompt = sft_format
         return prompt, text_content
 
-
     def batch_generate_inner(self, message, dataset, num_generations):
         prompt, prompt_text = self.prepare_inputs(message)
 
@@ -114,11 +113,11 @@ Do not miss objects. Output your visualization directly without explanation: '''
         patch_size = self.kwargs['patch_size']
 
         prompt_inputs = self.vl_chat_processor.tokenizer(
-                text=[prompt],
-                return_tensors="pt",
-                padding=True,
-                padding_side="right",
-                add_special_tokens=True
+            text=[prompt],
+            return_tensors="pt",
+            padding=True,
+            padding_side="right",
+            add_special_tokens=True
         )
         prompt_ids, prompt_mask = prompt_inputs["input_ids"], prompt_inputs["attention_mask"]
 
@@ -126,13 +125,12 @@ Do not miss objects. Output your visualization directly without explanation: '''
         prompt_mask = prompt_mask.repeat_interleave(num_generations, dim=0).to('cuda')
         input_embeds = self.model.language_model.get_input_embeddings()(prompt_ids)
 
-
         # TODO: if num_generations is too large, we need to split it into multiple batches
         if num_generations > 20:
             total_generations = []
             for i in range(prompt_ids.shape[0] // num_generations):
-                current_input_embeds = input_embeds[i*num_generations: (i+1)*num_generations]
-                current_attn_mask = prompt_mask[i*num_generations: (i+1)*num_generations]
+                current_input_embeds = input_embeds[i * num_generations: (i + 1) * num_generations]
+                current_attn_mask = prompt_mask[i * num_generations: (i + 1) * num_generations]
                 prompt_completion_ids = self.model.language_model.generate(
                     inputs_embeds=current_input_embeds,
                     attention_mask=current_attn_mask,
@@ -145,7 +143,7 @@ Do not miss objects. Output your visualization directly without explanation: '''
                 )
                 total_generations.append(prompt_completion_ids)
             prompt_completion_ids = torch.cat(total_generations, dim=0)
-        else: # if num_generations == 1, we directly generate all for the batch data
+        else:  # if num_generations == 1, we directly generate all for the batch data
             prompt_completion_ids = self.model.language_model.generate(
                 inputs_embeds=input_embeds,
                 attention_mask=prompt_mask,
@@ -161,7 +159,7 @@ Do not miss objects. Output your visualization directly without explanation: '''
         completion_ids = prompt_completion_ids
 
         image_gen_prompt_list = []
-        
+
         prompt = self.vl_chat_processor.tokenizer.decode(prompt_ids[0].cpu().tolist(), skip_special_tokens=True)
         for i in range(completion_ids.shape[0]):
             answer = self.vl_chat_processor.tokenizer.decode(completion_ids[i].cpu().tolist(), skip_special_tokens=True)
@@ -188,7 +186,7 @@ Do not miss objects. Output your visualization directly without explanation: '''
             padding=True,
             padding_side="right",
             add_special_tokens=True,
-        ) # {'input_ids', 'attention_mask'}
+        )  # {'input_ids', 'attention_mask'}
 
         prompt_ids, attention_mask = prompt_inputs["input_ids"], prompt_inputs["attention_mask"]
         prompt_ids = prompt_ids.to('cuda')
@@ -200,17 +198,19 @@ Do not miss objects. Output your visualization directly without explanation: '''
         attention_mask = torch.cat([attention_mask, attention_mask.new_ones((attention_mask.size(0), 1))], dim=1)
 
         inputs_embeds = self.model.language_model.get_input_embeddings()(prompt_ids)
-        pad_input_embeds = self.model.language_model.get_input_embeddings()(prompt_ids.new_full((1, 1), self.vl_chat_processor.pad_id))
+        pad_input_embeds = self.model.language_model.get_input_embeddings()(
+            prompt_ids.new_full((1, 1), self.vl_chat_processor.pad_id)
+        )
         total_generated_tokens_img = []
 
         # Currently only one image generation (since the diversity is low)
         for j in range(inputs_embeds.shape[0] // num_generations):
             # Make cond and uncond inputs embeds and attention mask
-            cond_inputs_embeds = inputs_embeds[j*num_generations: (j+1)*num_generations]
-            cond_attention_mask = attention_mask[j*num_generations: (j+1)*num_generations]
+            cond_inputs_embeds = inputs_embeds[j * num_generations: (j + 1) * num_generations]
+            cond_attention_mask = attention_mask[j * num_generations: (j + 1) * num_generations]
             uncond_inputs_embeds = cond_inputs_embeds.clone()
             uncond_inputs_embeds[:, 1:-1] = pad_input_embeds
-            
+
             inputs_embeds_img = torch.repeat_interleave(cond_inputs_embeds, 2, dim=0)
             inputs_embeds_img[1::2] = uncond_inputs_embeds
             attention_mask_img = torch.repeat_interleave(cond_attention_mask, 2, dim=0)
@@ -221,24 +221,27 @@ Do not miss objects. Output your visualization directly without explanation: '''
             for jj in range(0, inputs_embeds_img.shape[0], split_size):
                 start = jj
                 end = min(jj + split_size, inputs_embeds_img.shape[0])
-                generated_tokens = torch.zeros(((end-start)//2, image_token_num_per_image), dtype=torch.int64).cuda()
+                generated_tokens = torch.zeros((
+                    (end - start) // 2, image_token_num_per_image), dtype=torch.int64
+                ).cuda()
                 cur_inputs_embeds_img = inputs_embeds_img[start: end]
                 cur_attention_mask_img = attention_mask_img[start: end]
 
+                outputs = None
                 for k in range(image_token_num_per_image):
                     outputs = self.model.language_model.model(
-                        inputs_embeds=cur_inputs_embeds_img, 
-                        use_cache=True, 
-                        past_key_values=outputs.past_key_values if k != 0 else None, 
+                        inputs_embeds=cur_inputs_embeds_img,
+                        use_cache=True,
+                        past_key_values=None if k == 0 else outputs.past_key_values,
                         attention_mask=cur_attention_mask_img
                     )
-                    
+
                     hidden_states = outputs.last_hidden_state
                     logits = self.model.gen_head(hidden_states[:, -1, :])
                     logit_cond = logits[0::2, :]
                     logit_uncond = logits[1::2, :]
-                    
-                    logits = logit_uncond + cfg_weight * (logit_cond-logit_uncond)
+
+                    logits = logit_uncond + cfg_weight * (logit_cond - logit_uncond)
                     probs = torch.softmax(logits / temperature, dim=-1)
 
                     next_token = torch.multinomial(probs, num_samples=1)
@@ -247,13 +250,19 @@ Do not miss objects. Output your visualization directly without explanation: '''
                     next_token = torch.cat([next_token.unsqueeze(dim=1), next_token.unsqueeze(dim=1)], dim=1).view(-1)
                     img_embeds = self.model.prepare_gen_img_embeds(next_token)
                     cur_inputs_embeds_img = img_embeds.unsqueeze(dim=1)
-                    cur_attention_mask_img = torch.cat([cur_attention_mask_img, cur_attention_mask_img.new_ones((cur_attention_mask_img.shape[0], 1), dtype=torch.int)], dim=1)
+                    cur_attention_mask_img = torch.cat([
+                        cur_attention_mask_img,
+                        cur_attention_mask_img.new_ones((cur_attention_mask_img.shape[0], 1), dtype=torch.int)
+                    ], dim=1)
 
                 total_generated_tokens_img.append(generated_tokens)
 
         total_generated_tokens_img = torch.cat(total_generated_tokens_img, dim=0)
 
-        dec = self.model.gen_vision_model.decode_code(generated_tokens.to(dtype=torch.int), shape=[num_generations, 8, img_size//patch_size, img_size//patch_size])
+        dec = self.model.gen_vision_model.decode_code(
+            generated_tokens.to(dtype=torch.int),
+            shape=[num_generations, 8, img_size // patch_size, img_size // patch_size]
+        )
         dec = dec.to(torch.float32).cpu().numpy().transpose(0, 2, 3, 1)
 
         dec = np.clip((dec + 1) / 2 * 255, 0, 255)
@@ -262,4 +271,3 @@ Do not miss objects. Output your visualization directly without explanation: '''
         visual_img[:, :, :] = dec
 
         return [PIL.Image.fromarray(img) for img in visual_img]
-
